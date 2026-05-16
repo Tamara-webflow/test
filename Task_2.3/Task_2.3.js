6231bb6e6150daef809d39f2ab74d4a74c43c1ff
Tamara-webflow <romanenkova.web@gmail.com>
2026-05-16 10:17:21 +0200

    Add lessons_2.3

---
+++ Task_2.3/Task_2.3.js
@@ -0,0 +1,54 @@
+let titleProject = "Оценка стоимости";
+let screensValue = "шаблонные, с уникальным дизайном, с анимациями";
+let screenPrice = 5000;
+let percentage = 80;
+let fullPrice = 100000;
+let responsive = true;
+console.log(titleProject);
+console.log(screensValue);
+console.log(screenPrice);
+console.log(percentage);
+console.log(fullPrice);
+console.log(responsive);
+
+
+titleProject = prompt('Название проекта?');
+console.log(titleProject);
+screensValue = prompt('Какие типы экранов? (шаблонные, с уникальным дизайном, с анимациями)');
+console.log(screensValue);
+responsive = confirm('Нужен ли адаптивный сайт?');
+console.log(responsive);
+
+let service1 = prompt('Какой сервис нужен?');
+console.log(service1);
+let servicePrice1 = prompt('Сколько это будет стоить?');
+console.log(servicePrice1);
+let service2 = prompt('Какой еще сервис тебе нужен?');
+console.log(service2);
+let servicePrice2 = prompt('Сколько это будет стоить?');
+console.log(servicePrice2);
+
+fullPrice = +servicePrice1 + +servicePrice2 + screenPrice;
+console.log(fullPrice);
+let servicePercentPrice = Math.round(fullPrice - fullPrice * 0.15);
+console.log(servicePercentPrice);
+
+if (fullPrice > 50000) {
+    console.log('Даем скидку в 10%');
+} else if (fullPrice === 50000) {
+    console.log('Даем скидку в 10%');
+}
+else if (fullPrice > 20000) {
+    console.log('Даем скидку в 5%');
+} else if (fullPrice === 20000) {
+    console.log('Даем скидку в 5%');
+}
+else if (fullPrice > 0 ) {
+    console.log('Cкидка не предусмотрена');
+} else if (fullPrice === 0) {
+    console.log('Cкидка не предусмотрена');
+}
+
+else {
+    console.log('Что то пошло не так');
+}
---
+++ lesson_2.3/lesson_2.3.js
@@ -0,0 +1,31 @@
+let x = 5;
+
+if (x > 10) {
+    console.log("Условие верно"); 
+} else {
+    console.log("Условие неверно");
+}  
+
+    let a =5;
+    let b ='10';
+    let res = a + +b;
+console.log(a);
+console.log(b);
+console.log(res);
+
+console.log(typeof a); // number
+console.log(typeof b); // string    
+
+    console.log(a + b); // 510
+    console.log(a +b); // 50
+    console.log(a.toString()); // '5'
+    console.log(typeof a); // number
+
+// alert('Привет, мир!');
+
+let age = +prompt('Сколько вам лет?');
+console.log(age);   
+
+let c = 10;
+let res2 = age + c;
+console.log(res2);
